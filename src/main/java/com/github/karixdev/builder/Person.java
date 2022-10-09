25144715ce06f5d5b08e4238118f5ced8adb29f2
karixdev <30869888+karixdev@users.noreply.github.com>
2022-10-09 19:45:23 +0200

    Created Person class

---
+++ src/main/java/com/github/karixdev/builder/Person.java
@@ -0,0 +1,13 @@
+package com.github.karixdev.builder;
+
+import lombok.Data;
+
+@Data
+public class Person {
+    private String forename;
+    private String surname;
+    private int age;
+    private int height;
+    private double weight;
+    private String hairColor;
+}
