ffb924a9274dddea11ed583d58326d6bb53f22bc
karixdev <30869888+karixdev@users.noreply.github.com>
2022-10-09 20:04:29 +0200

    Created Dog class

---
+++ src/main/java/com/github/karixdev/builder/Dog.java
@@ -0,0 +1,14 @@
+package com.github.karixdev.builder;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Dog {
+    private String name;
+    private int age;
+    private String breed;
+    private String color;
+    private double weight;
+}
