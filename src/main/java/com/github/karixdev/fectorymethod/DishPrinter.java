2bbe4305cf7772aed9ca58636eb41529c057e2f7
karixdev <30869888+karixdev@users.noreply.github.com>
2022-10-14 23:33:08 +0200

    Created dish printer

---
+++ src/main/java/com/github/karixdev/fectorymethod/DishPrinter.java
@@ -0,0 +1,7 @@
+package com.github.karixdev.fectorymethod;
+
+public class DishPrinter {
+    public static void printPreparedDish(String cuisine, DishType dishType) {
+        System.out.println("Prepared " + cuisine + " " + dishType);
+    }
+}
