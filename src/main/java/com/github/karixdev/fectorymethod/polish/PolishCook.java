ff5c9404011755208b74723b0f082caa2ae433dd
karixdev <30869888+karixdev@users.noreply.github.com>
2022-10-14 23:34:46 +0200

    Polish cook uses DishPrinter to print prepared dish

--- src/main/java/com/github/karixdev/fectorymethod/polish/PolishCook.java
+++ src/main/java/com/github/karixdev/fectorymethod/polish/PolishCook.java
@@ -1,11 +1,12 @@
 package com.github.karixdev.fectorymethod.polish;
 
 import com.github.karixdev.fectorymethod.Cook;
+import com.github.karixdev.fectorymethod.DishPrinter;
 import com.github.karixdev.fectorymethod.DishType;
 
 public class PolishCook implements Cook {
     @Override
     public void prepareDish(DishType dishType) {
-        System.out.println("Prepared polish " + dishType);
+        DishPrinter.printPreparedDish("polish", dishType);
     }
 }
