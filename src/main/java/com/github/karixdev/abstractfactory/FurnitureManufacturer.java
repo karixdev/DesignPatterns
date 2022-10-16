44d0cf05388361188565e54cd51b899b86179f51
karixdev <30869888+karixdev@users.noreply.github.com>
2022-10-16 22:04:47 +0200

    Created abstract furniture manufacturer

---
+++ src/main/java/com/github/karixdev/abstractfactory/FurnitureManufacturer.java
@@ -0,0 +1,6 @@
+package com.github.karixdev.abstractfactory;
+
+public abstract class FurnitureManufacturer {
+    public abstract Armchair createArmchair();
+    public abstract CoffeeTable createCoffeeTable();
+}
