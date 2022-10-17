dad60a4284b966863f968c1acbae0d6e4221139c
karixdev <30869888+karixdev@users.noreply.github.com>
2022-10-17 23:41:20 +0200

    Created american and eu phone charger classes

---
+++ src/main/java/com/github/karixdev/adapter/american/AmericanPhoneCharger.java
@@ -0,0 +1,7 @@
+package com.github.karixdev.adapter.american;
+
+public class AmericanPhoneCharger {
+    public void connect(AmericanOutlet americanOutlet) {
+        americanOutlet.connectAmericanPlug();
+    }
+}
---
+++ src/main/java/com/github/karixdev/adapter/european/EuPhoneCharger.java
@@ -0,0 +1,7 @@
+package com.github.karixdev.adapter.european;
+
+public class EuPhoneCharger {
+    public void connect(EuropeanOutlet europeanOutlet) {
+        europeanOutlet.connectEuropeanPlug();
+    }
+}
