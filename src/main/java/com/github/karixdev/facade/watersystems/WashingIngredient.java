28b2161d706f224f491ec916e5c9824f34618fc0
karixdev <kari.klucz@gmail.com>
2022-10-27 14:51:12 +0200

    Created WashingIngredient class

---
+++ src/main/java/com/github/karixdev/facade/watersystems/WashingIngredient.java
@@ -0,0 +1,12 @@
+package com.github.karixdev.facade.watersystems;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class WashingIngredient {
+    public String name;
+}
