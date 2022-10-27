0527609d906081ab629e08405896a48eec85555e
karixdev <kari.klucz@gmail.com>
2022-10-27 15:01:46 +0200

    Added lombok builder annotation to LaundryItem

--- src/main/java/com/github/karixdev/facade/laundry/LaundryItem.java
+++ src/main/java/com/github/karixdev/facade/laundry/LaundryItem.java
@@ -1,10 +1,12 @@
 package com.github.karixdev.facade.laundry;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
+@Builder
 @AllArgsConstructor
 @NoArgsConstructor
 public class LaundryItem {
