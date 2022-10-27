8705dc23903acd67c0a4083cd2f8be22eeb7c5fe
karixdev <kari.klucz@gmail.com>
2022-10-27 14:54:07 +0200

    Created WaterPump class

---
+++ src/main/java/com/github/karixdev/facade/watersystems/WashingMachineWaterPump.java
@@ -0,0 +1,7 @@
+package com.github.karixdev.facade.watersystems;
+
+public class WashingMachineWaterPump {
+    public void pump() {
+        System.out.println("Pumping water into washing machine");
+    }
+}
